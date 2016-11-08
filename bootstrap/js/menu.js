721a7384167367cb42a57a6e19d562f8d93eb87b
FG1511NO07\jeongkh <jeongkh@yellofg.com>
2016-11-08 19:09:31 +0900

    bootstrap

---
+++ bootstrap/js/menu.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#yfg a").click(function(){
+    $("#yfg1").load("yfg.html");
+    $("#div1").load("ceo.html");
+  });
+  $("#ydw a").click(function(){
+    $("#yfg1").load("ydw.html");
+  });
+  $("#ymp a").click(function(){
+    $("#yfg1").load("ymp.html");
+  });
+});
